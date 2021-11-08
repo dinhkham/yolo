5b1028370bd2d39b37063a1f3f2c409dacfe5be9
tungxdnd <dinhkhamhubt@gmail.com>
2021-11-08 23:57:51 +0700

    push code yolo day 1

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+$(function () {
+    
+    if ($('.yolo_slider').length > 0) {
+        var $owl = $('.yolo_slider');
+        $owl.owlCarousel({
+            loop: true,
+            nav: true,
+            dots: true,
+            navText : ["<i class='bx bx-chevron-left' ></i>","<i class='bx bx-chevron-right' ></i>"],
+            items: 1
+        });
+    }
+    
+    if ($('.mobile_menu').length > 0) {
+        $(".mobile_menu").slideMobileMenu({
+
+            onMenuLoad: function (menu) {
+    
+                console.log(menu)
+    
+            },
+    
+            onMenuToggle: function (menu, opened) {
+    
+                console.log(opened)
+    
+            }
+    
+        });
+    }
+    var $ww = $(window).width();
+    if($ww>1024){
+        $(window).scroll(function(){
+            scroll = $(window).scrollTop();
+            if (scroll >= 35){
+                $('#header').addClass('fixed');
+            }
+            else{
+                $('#header').removeClass('fixed');
+            }
+        });
+    
+    }
+    
+});
